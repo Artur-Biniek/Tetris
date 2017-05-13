69f85b8b35ed07df08d741a7bae0be3ad277fc6b
Artur-Biniek <artur.biniek@gmail.com>
2017-05-13 21:02:12 +0200

    First version finished.

---
+++ src/Tetris/MainWindow.xaml.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Tetris
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const int PIXEL_SIZE = 15;
+
+        const int WIDTH = 32;
+        const int HEIGHT = 32;
+
+        DispatcherTimer _dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+
+        Rectangle[,] _pixels = new Rectangle[32, 32];
+
+        uint[] _screenMemory = new uint[32];
+        uint[] TotalLines = new uint[1];
+
+        long _programStart;
+        uint _gamePad;
+
+        private TetrisGame _game;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            initializeScreen();
+            initializeTimer();
+            InitilizeKeyboard();
+
+            _game = new TetrisGame(_screenMemory, TotalLines, getTime, () => _gamePad);
+
+            _game.InitGame();
+        }
+
+        private void InitilizeKeyboard()
+        {
+            this.KeyDown += (o, e) =>
+            {
+                switch (e.Key)
+                {
+                    case System.Windows.Input.Key.Left:
+                        _gamePad |= TetrisGame.KEY_LEFT;
+                        break;
+                    case System.Windows.Input.Key.Right:
+                        _gamePad |= TetrisGame.KEY_RIGHT;
+                        break;
+                    case System.Windows.Input.Key.Up:
+                        _gamePad |= TetrisGame.KEY_UP;
+                        break;
+                    case System.Windows.Input.Key.Down:
+                        _gamePad |= TetrisGame.KEY_DOWN;
+                        break;
+                    case System.Windows.Input.Key.Escape:
+                        _gamePad |= TetrisGame.KEY_ESC;
+                        break;
+                }
+            };
+
+            this.KeyUp += (o, e) =>
+            {
+                switch (e.Key)
+                {
+                    case System.Windows.Input.Key.Left:
+                        _gamePad &= ~TetrisGame.KEY_LEFT;
+                        break;
+                    case System.Windows.Input.Key.Right:
+                        _gamePad &= ~TetrisGame.KEY_RIGHT;
+                        break;
+                    case System.Windows.Input.Key.Up:
+                        _gamePad &= ~TetrisGame.KEY_UP;
+                        break;
+                    case System.Windows.Input.Key.Down:
+                        _gamePad &= ~TetrisGame.KEY_DOWN;
+                        break;
+                    case System.Windows.Input.Key.Escape:
+                        _gamePad &= ~TetrisGame.KEY_ESC;
+                        break;
+                }
+            };
+        }
+
+        private void initializeTimer()
+        {
+            _programStart = DateTime.Now.Ticks;
+
+            _dispatcherTimer.Interval = TimeSpan.FromMilliseconds(0.2);
+            _dispatcherTimer.Tick += gameLoop;
+
+            _dispatcherTimer.IsEnabled = true;
+        }
+
+        private void gameLoop(object sender, EventArgs e)
+        {
+            _game.Run();
+
+            screenRefresh();
+
+            if (_game.GameOver)
+            {
+                Title = "Game Over! " + TotalLines[0].ToString();
+            }
+            else
+            {
+                Title = TotalLines[0].ToString();
+            }
+        }
+
+        private void initializeScreen()
+        {
+            for (int i = 0; i < 32; i++)
+            {
+                var def = new RowDefinition();
+                def.Height = GridLength.Auto;
+
+                ScreenGrid.RowDefinitions.Add(def);
+
+                var colDef = new ColumnDefinition();
+                colDef.Width = GridLength.Auto;
+                ScreenGrid.ColumnDefinitions.Add(colDef);
+            }
+
+            for (int i = 0; i < 32; i++)
+            {
+                ScreenGrid.RowDefinitions.Add(new RowDefinition());
+
+                for (int j = 0; j < 32; j++)
+                {
+                    var rect = new Rectangle();
+
+                    rect.Width = PIXEL_SIZE;
+                    rect.Height = PIXEL_SIZE;
+                    rect.Fill = new SolidColorBrush(Colors.Black);
+
+                    rect.Stroke = new SolidColorBrush(Colors.DarkRed);
+                    rect.StrokeThickness = 1;
+
+                    Grid.SetRow(rect, i);
+                    Grid.SetColumn(rect, j);
+
+                    ScreenGrid.Children.Add(rect);
+
+                    _pixels[i, j] = rect;
+                }
+            }
+        }
+
+        private void screenRefresh()
+        {
+            for (int row = 0; row < 32; row++)
+            {
+                var bit = (uint)1 << 31;
+
+                for (var col = 0; col < 32; col++, bit >>= 1)
+                {
+                    if ((_screenMemory[row] & bit) != 0)
+                    {
+                        _pixels[row, col].Fill = new SolidColorBrush(Colors.Lime);
+                    }
+                    else
+                    {
+                        _pixels[row, col].Fill = new SolidColorBrush(Colors.Black);
+                    }
+                }
+            }
+        }
+
+        private uint getTime()
+        {
+            return (uint)TimeSpan.FromTicks(DateTime.Now.Ticks - _programStart).TotalMilliseconds;
+        }
+    }
+}
---
+++ src/Tetris/TetrisGame.cs
@@ -0,0 +1,400 @@
+﻿using System;
+
+namespace Tetris
+{
+    public class TetrisGame
+    {
+        public const uint KEY_LEFT = 1 << 0;
+        public const uint KEY_UP = 1 << 1;
+        public const uint KEY_RIGHT = 1 << 2;
+        public const uint KEY_DOWN = 1 << 3;
+        public const uint KEY_ESC = 1 << 4;
+
+        private readonly uint[] BLOCKS_DATA = new[]
+        {
+               (uint)0x0F00, (uint)0x2222, (uint)0x00f0, (uint)0x4444, // I-block
+               (uint)0x8E00, (uint)0x6440, (uint)0x0E20, (uint)0x44C0, // J-block
+               (uint)0x2E00, (uint)0x4460, (uint)0x0E80, (uint)0xC440, // L-block
+               (uint)0x6600, (uint)0x6600, (uint)0x6600, (uint)0x6600, // O-block
+               (uint)0x6C00, (uint)0x4620, (uint)0x06C0, (uint)0x8C40, // S-block
+               (uint)0x4E00, (uint)0x4640, (uint)0x0E40, (uint)0x4C40, // T-block
+               (uint)0xC600, (uint)0x2640, (uint)0x0C60, (uint)0x4C80  // Z-block
+        };
+
+        private readonly int[] START_LOCATION_DATA = new[]
+        {
+            -1, 0, -2, 0,
+             0, 0, -1, 0,
+             0, 0, -1, 0,
+             0, 0,  0, 0,
+             0, 0, -1, 0,
+             0, 0, -1, 0,
+             0, 0, -1, 0
+        };
+
+        const uint FULL_LINE_MASK = 0x7FE00000;
+        const uint COPY_LINE_MASK = 0xFFF00000;
+
+        const uint REPEAT_RATE_SLOW = 200;
+        const uint REPEAT_RATE_FAST = 100;
+        const uint REPEAT_RATE_RAPID = 40;
+
+        private readonly Random _rnd = new Random();
+
+        uint[] _board = new uint[32];
+
+        int _curRow, _curCol;
+        int _curDelay = 1000;
+
+        int _curBlock, _curRotation;
+        int _nextBlock, _nextRotation;
+
+        bool _playing;
+
+        uint _oldKeys;
+        uint _lastKeyboardTime;
+        uint _lastUpdateTime;
+
+        private uint[] _screenMemory;
+        private uint[] _sevenSegDisplay;
+        private Func<uint> _getTime;
+        private Func<uint> _getKeys;
+
+        public bool GameOver
+        {
+            get { return !_playing; }
+        }
+
+        public TetrisGame(uint[] screenMemory, uint[] sevenSegDisplay, Func<uint> getElapsedMiliseconds, Func<uint> getGamepadState)
+        {
+            _screenMemory = screenMemory;
+            _sevenSegDisplay = sevenSegDisplay;
+            _getTime = getElapsedMiliseconds;
+            _getKeys = getGamepadState;
+        }
+
+        public void InitGame()
+        {
+            _sevenSegDisplay[0] = 0;
+
+            _nextBlock = _rnd.Next(7) * 4;
+            _nextRotation = _rnd.Next(4);
+
+            createBoard();
+            generateTetromino();
+
+            blitBlock(_curRow, _curCol, _curRotation);
+
+            _playing = true;
+        }
+
+        public void Run()
+        {
+            var keystate = _getKeys();
+
+            if ((keystate & KEY_ESC) != 0)
+            {
+                InitGame();
+            }
+
+            if (!_playing) return;
+
+            var time = _getTime();
+
+            bool moveLeft = false, moveRight = false, rotate = false, moveDown = false;
+            int ncol = _curCol, nrow = _curRow, nrot = _curRotation;
+
+            if ((keystate & KEY_LEFT) != 0)
+            {
+                if ((_oldKeys & KEY_LEFT) == 0)
+                {
+                    moveLeft = true;
+                    _lastKeyboardTime = time + REPEAT_RATE_SLOW;
+                }
+                else
+                {
+                    if (time > _lastKeyboardTime)
+                    {
+                        _lastKeyboardTime = time + REPEAT_RATE_FAST;
+                        moveLeft = true;
+                    }
+                }
+            }
+
+            if ((keystate & KEY_RIGHT) != 0)
+            {
+                if ((_oldKeys & KEY_RIGHT) == 0)
+                {
+                    moveRight = true;
+                    _lastKeyboardTime = time + REPEAT_RATE_SLOW;
+                }
+                else
+                {
+                    if (time > _lastKeyboardTime)
+                    {
+                        _lastKeyboardTime = time + REPEAT_RATE_FAST;
+                        moveRight = true;
+                    }
+                }
+            }
+
+            if ((keystate & KEY_UP) != 0)
+            {
+                if ((_oldKeys & KEY_UP) == 0)
+                {
+                    rotate = true;
+                    _lastKeyboardTime = time + REPEAT_RATE_SLOW;
+                }
+                else
+                {
+                    if (time > _lastKeyboardTime)
+                    {
+                        _lastKeyboardTime = time + REPEAT_RATE_FAST;
+                        rotate = true;
+                    }
+                }
+            }
+
+            if ((keystate & KEY_DOWN) != 0)
+            {
+                if ((_oldKeys & KEY_DOWN) == 0)
+                {
+                    moveDown = true;
+                    _lastKeyboardTime = time + REPEAT_RATE_SLOW;
+                }
+                else
+                {
+                    if (time > _lastKeyboardTime)
+                    {
+                        _lastKeyboardTime = time + REPEAT_RATE_RAPID;
+                        moveDown = true;
+                    }
+                }
+            }
+
+            _oldKeys = keystate;
+
+            if (moveLeft)
+            {
+                ncol--;
+            }
+            if (moveRight)
+            {
+                ncol++;
+            }
+            if (rotate)
+            {
+                nrot = (_curRotation + 1) % 4;
+            }
+            if (moveDown)
+            {
+                nrow++;
+            }
+
+            if (moveLeft || moveRight || rotate || moveDown)
+            {
+                if (canMoveBlock(ncol, nrow, nrot))
+                {
+                    _curCol = ncol;
+                    _curRow = nrow;
+                    _curRotation = nrot;
+                }
+            }
+
+            if (time > _lastUpdateTime + _curDelay)
+            {
+                if (canMoveBlock(_curCol, _curRow + 1, _curRotation))
+                {
+                    _curRow++;
+                }
+                else
+                {
+                    mergeBlock();
+                    _sevenSegDisplay[0] += checkLines();
+                    generateTetromino();
+                }
+
+                _lastUpdateTime = time;
+            }
+
+            blitBlock(_curRow, _curCol, _curRotation);
+        }
+
+        private uint checkLines()
+        {
+            uint res = 0;
+
+            for (int row = 19; row >= 0; row--)
+            {
+                if ((_board[row] & FULL_LINE_MASK) == FULL_LINE_MASK)
+                {
+                    for (int next = row; next > 1; next--)
+                    {
+                        _board[next] = (_board[next - 1] & COPY_LINE_MASK);
+                    }
+
+                    row++; // recheck current row;
+                    res++; // add line to result
+                }
+            }
+
+            return res;
+        }
+
+        private void mergeBlock()
+        {
+            uint mask = 0xF000;
+            int offset = 16;
+
+            uint mem = 0u;
+            uint strip = 0u;
+            uint block = BLOCKS_DATA[_curBlock + _curRotation];
+            int i = 0;
+
+            for (i = _curRow; i < _curRow + 4; i++)
+            {
+                mem = _board[i];
+
+                strip = (uint)(block & mask) << offset;
+
+                if (_curCol < 0)
+                {
+                    strip <<= (-_curCol);
+                }
+                else
+                {
+                    strip >>= _curCol;
+                }
+
+                mem |= strip;
+
+                _board[i] = mem;
+
+                mask >>= 4;
+                offset += 4;
+            }
+        }
+
+        private bool canMoveBlock(int dcol, int drow, int drot)
+        {
+            uint mask = 0x8000;
+            uint block = BLOCKS_DATA[_curBlock + drot];
+            int x, y;
+
+            for (int r = 0; r < 4; r++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    if ((mask & block) != 0)
+                    {
+                        x = c + dcol;
+                        y = r + drow;
+                        if (x < 0) return false;
+                        if (x > 11) return false;
+                        if (y < 0) return false;
+                        if (y > 20) return false;
+
+                        var line = _board[y];
+
+                        var bit = 31 - x;
+
+                        if ((line & (1 << bit)) != 0) return false;
+                    }
+
+                    mask >>= 1;
+                }
+            }
+
+            return true;
+        }
+
+        private void createBoard()
+        {
+            for (int i = 0; i < 32; i += 1)
+            {
+                _screenMemory[i] = 1;
+            }
+
+            for (int i = 0; i < 20; i += 1)
+            {
+                _board[i] = 0x80100000;
+            }
+
+            _board[20] = 0xFFF00000;
+        }
+
+        private void blitBlock(int row, int col, int rot)
+        {
+            uint mask = 0xF000;
+            int offset = 16;
+
+            uint mem = 0u;
+            uint strip = 0u;
+            uint block = BLOCKS_DATA[_curBlock + rot];
+            //uint next = BLOCKS_DATA[NextBlock + NextRotation];
+            int i = 0;
+
+            for (i = 0; i < row; i++)
+            {
+                _screenMemory[i] = _board[i];
+            }
+
+            for (i = row; i < row + 4; i++, mask >>= 4, offset += 4)
+            {
+                if (i < 0) continue;
+
+                mem = _board[i];
+
+                strip = (uint)(block & mask) << offset;
+
+                if (col < 0)
+                {
+                    strip <<= (-col);
+                }
+                else
+                {
+                    strip >>= col;
+                }
+
+                mem |= strip;
+
+                _screenMemory[i] = mem;
+
+
+                /*
+                mem = _screenMemory[i - row + 1];
+
+                strip = (uint)(next & mask) << offset;
+
+                strip >>= 14;
+
+                mem |= strip;
+
+                _screenMemory[i - row + 1] = mem;
+                */
+            }
+
+            for (i = row + 4; i < 32; i++)
+            {
+                _screenMemory[i] = _board[i];
+            }
+        }
+
+        private void generateTetromino()
+        {
+            _curBlock = _nextBlock;
+            _curRotation = _nextRotation;
+
+            _nextBlock = _rnd.Next(7) * 4;
+            _nextRotation = _rnd.Next(4);
+
+            _curRow = START_LOCATION_DATA[_curBlock + _curRotation];
+            _curCol = 4;
+
+            if (!canMoveBlock(_curCol, _curRow, _curRotation))
+            {
+                _playing = false;
+            }
+        }
+    }
+}
